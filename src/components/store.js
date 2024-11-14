8f1938cc9a2383f6bbdfa221ed33b26237b8ecea
IraTokar <tokar.ira20@gmail.com>
2024-11-14 22:21:35 +0100

    add middlrware

--- src/components/store.js
+++ src/components/store.js
@@ -8,7 +8,14 @@ export const store = configureStore({
     reducer: {
         contacts: contactsReducer,
         filter: filterReducer,
-    }
+    },
+
+    middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
